20d2c61eb9ef7b5c0af3477d52dd8aca2c207164
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-07-23 18:23:14 -0300

    Fix - refatoração do código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,34 +9,26 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { ModalDetailsCharactersComponent } from './components/characters/modal-details-characters/modal-details-characters.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatListModule } from '@angular/material/list';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ComicsComponent } from './components/comics/comics.component';
 import { EventsComponent } from './components/events/events.component';
-import { SeriesComponent } from './components/series/series.component';
 import { CharactersComponent } from './components/characters/characters.component';
-import { ModalDetailsComicsComponent } from './components/comics/modal-details-comics/modal-details-comics.component';
-import { ModalDetailsEventsComponent } from './components/events/modal-details-events/modal-details-events.component';
-import { ModalDetailsSeriesComponent } from './components/series/modal-details-series/modal-details-series.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ModalDetailsCharactersComponent } from './components/characters/modal-details-characters/modal-details-characters.component';
+import { ModalDetailsEventsComponent } from './components/events/modal-details-events/modal-details-events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CharactersComponent,
     ModalDetailsCharactersComponent,
-    NavbarComponent,
-    ComicsComponent,
-    EventsComponent,
-    SeriesComponent,
-    ModalDetailsComicsComponent,
     ModalDetailsEventsComponent,
-    ModalDetailsSeriesComponent,
+    NavbarComponent,
+    EventsComponent,
   ],
   imports: [
     BrowserModule,
